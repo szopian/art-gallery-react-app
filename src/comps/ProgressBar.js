a44c92c3b5d69a80bbc5c227c58a981b60e8db39
Adri Ana <szopiangreg@gmail.com>
2021-03-11 22:11:19 +0000

    initial commit create timestamp in database

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
-const ProgressBar = () => {
-  return <div className="progress-bar">progress</div>;
+const ProgressBar = ({ file, setFile }) => {
+  const { url, progress } = useStorage(file);
+
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
