55c8a3d112b18270a479a7220b70fbc5a8765ef3
Adri Ana <szopiangreg@gmail.com>
2021-03-12 00:41:37 +0000

    commit

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>GSGallery</h1>
+      <h2>Art Gallery</h2>
+      <p>“Creativity takes courage”</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
