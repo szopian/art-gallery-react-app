a130b2c382ab536f88ddc04c3cc8b857fde7b52b
Adri Ana <szopiangreg@gmail.com>
2021-03-12 00:05:09 +0000

    add animation, onClick modal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageGrid from "./comps/imageGrid";
 import Title from "./comps/Title";
 import UploadForm from "./comps/UploadForm";
+import Modal from "./comps/Modal";
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
